2eb4be2645a15b60df6c2bf53829d2eccdc38cd1
Pebblezzzz <formchatbots@gmail.com>
2026-09-16 20:51:45 +0100

    Correct measured water first-step regression

--- src/test/java/dev/phantom/ac/Phase5MechanicsTest.java
+++ src/test/java/dev/phantom/ac/Phase5MechanicsTest.java
@@ -43,7 +43,7 @@ class Phase5MechanicsTest {
     var physics=new Vanilla12111Physics();
     var water=physics.step(new PhysicsContext(4,Player.initial(Vec3.ZERO),new AdvancedInput(1,0,false,true,false),air(),Simulation.Environment.WATER,new Attributes(.1),Phase5Mechanics.MovementEffects.NONE,Phase5Mechanics.Pose.STANDING,Phase5Mechanics.MovementEnvironment.vanillaWater(false,true,false,true)));
     var lava=physics.step(new PhysicsContext(5,Player.initial(Vec3.ZERO),new AdvancedInput(1,0,false),air(),Simulation.Environment.LAVA,new Attributes(.1),Phase5Mechanics.MovementEffects.NONE,Phase5Mechanics.Pose.STANDING,Phase5Mechanics.MovementEnvironment.vanillaLava(true,false,false)));
-    assertEquals(0.0098, water.state().velocity().x(), 1e-12);
+    assertEquals(0.01764, water.state().velocity().x(), 1e-12);
     assertEquals(0.0098, lava.state().velocity().x(), 1e-12);
     assertEquals(-0.02, lava.state().velocity().y(), 1e-12);
   }
