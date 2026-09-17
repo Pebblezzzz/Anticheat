7f5cab35ea0dc38e313478f26f1f6c73d835c008
Pebblezzzz <formchatbots@gmail.com>
2026-09-18 00:52:43 +0100

    Align regression tests with corrected physics

--- src/test/java/dev/phantom/ac/Phase5MechanicsTest.java
+++ src/test/java/dev/phantom/ac/Phase5MechanicsTest.java
@@ -15,7 +15,12 @@ import dev.phantom.ac.Simulation.Vanilla12111Physics;
 import dev.phantom.ac.State.Player;
 
 class Phase5MechanicsTest {
-  private static World.Snapshot air() { return World.Snapshot.emptyVisibleChunks(List.of(new World.Chunk(-1,-1),new World.Chunk(-1,0),new World.Chunk(0,-1),new World.Chunk(0,0))); }
+  private static World.Snapshot air() {
+    var stone=dev.phantom.ac.world.v12111.BlockCatalogue12111.decode("minecraft:stone",java.util.Map.of());
+    var b=World.Snapshot.builder(Contracts.TARGET_VERSION).loadChunk(-1,-1).loadChunk(-1,0).loadChunk(0,-1).loadChunk(0,0);
+    for(int x=-8;x<=8;x++)for(int z=-8;z<=8;z++)b.setBlock(x,-1,z,stone);
+    return b.build();
+  }
   @Test void sprintAndSneakAreExplicitInputs() {
     var physics=new Vanilla12111Physics();
     var state=Player.initial(Vec3.ZERO);
