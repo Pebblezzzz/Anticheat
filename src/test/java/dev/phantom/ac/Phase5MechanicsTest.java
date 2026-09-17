d36e28f0717adef1ed2b850408ab392f3441752a
Pebblezzzz <formchatbots@gmail.com>
2026-09-18 00:55:58 +0100

    Fix test collection imports

--- src/test/java/dev/phantom/ac/Phase5MechanicsTest.java
+++ src/test/java/dev/phantom/ac/Phase5MechanicsTest.java
@@ -1,6 +1,8 @@
 package dev.phantom.ac;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
