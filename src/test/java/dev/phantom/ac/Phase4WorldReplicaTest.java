8af383759a55f388b7e70688dd07711d1cf13995
Pebblezzzz <formchatbots@gmail.com>
2026-09-23 15:05:11 +0100

    test: cover pending world state during movement validation

--- src/test/java/dev/phantom/ac/Phase4WorldReplicaTest.java
+++ src/test/java/dev/phantom/ac/Phase4WorldReplicaTest.java
@@ -110,6 +110,26 @@ final class Phase4WorldReplicaTest {
   }
 
 
+  @Test void pendingWorldSnapshotIncludesSentMutationsBeforeAck(){
+    var r=new Phase4WorldReplica(V);
+    var stone=BlockCatalogue12111.decode("minecraft:stone",Map.of());
+    var air=BlockState.air();
+    var chunk=new Chunk(0,0);
+    var position=new Pos(0,64,0);
+
+    r.accept(new Phase4WorldReplica.ChunkData(
+        o(1,1),p(1,1),chunk,Map.of(position,stone)));
+
+    r.queue(new Phase4WorldReplica.BlockChange(
+        o(2,2),p(2,2),position,air));
+    r.openBarrier((short)-2);
+
+    assertEquals(stone,r.snapshotAtOrBefore(3).blockAtOrNull(0,64,0));
+    assertNull(r.snapshotAtOrBeforeIncludingPending(3).blockAtOrNull(0,64,0));
+    assertEquals(Coverage.KNOWN,
+        r.snapshotAtOrBeforeIncludingPending(3).coverageAt(0,64,0));
+  }
+
   @Test void packedSectionRoundTripsAcrossLongBoundary(){
     BlockState[] states=new BlockState[4096];
     for(int i=0;i<states.length;i++)
